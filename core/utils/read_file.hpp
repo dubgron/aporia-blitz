3d3e32ee0b334ad1f03dcab624b3c970f22150b0
Patryk Skwiercz <mrdubgron@gmail.com>
2019-09-20 22:47:29 +0200

    refactor: Improved passing a string with a filename

--- core/utils/read_file.hpp
+++ core/utils/read_file.hpp
@@ -5,9 +5,9 @@
 
 namespace Aporia::Utils
 {
-    inline std::string read_file(const std::string& path)
+    inline std::string read_file(std::string path)
     {
-        std::ifstream file(path, std::ios::binary);
+        std::ifstream file(std::move(path), std::ios::binary);
         return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     }
 }
