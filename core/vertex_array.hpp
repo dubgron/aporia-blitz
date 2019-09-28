0fbb40f4a4c4c5eab8025fb25fba680dd8cb7909
Patryk Skwiercz <mrdubgron@gmail.com>
2019-09-28 17:32:54 +0200

    feat: Added VertexArray class

---
+++ core/vertex_array.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <array>
+#include <vector>
+
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/PrimitiveType.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <SFML/Graphics/Vertex.hpp>
+
+#include "platform.hpp"
+
+namespace Aporia
+{
+    class APORIA_API VertexArray : public sf::Drawable
+    {
+    public:
+        VertexArray(sf::PrimitiveType primitive_type = sf::Quads, size_t size = 10000);
+
+        void add(const std::array<sf::Vertex, 4>& vertecies);
+        void clear();
+
+        size_t size() const;
+
+    private:
+        std::vector<sf::Vertex> _vertices;
+        sf::PrimitiveType _primitive_type;
+
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    };
+}
