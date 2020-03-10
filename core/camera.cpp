c3e1c7e905a9f5e0676dae0b42b555af85545c8f
Patryk Skwiercz <mrdubgron@gmail.com>
2020-03-10 23:10:45 +0100

    feat: Added View and ViewProjection matrices as fields
    Previously those matrices where calculated everyframe,
    now they are recalculated everytime they are modified.

--- core/camera.cpp
+++ core/camera.cpp
@@ -1,65 +1,87 @@
 #include "camera.hpp"
 
+#include <cmath>
+
 namespace Aporia
 {
     Camera::Camera(float left, float right, float bottom, float top)
+        : _left(left), _right(right), _bottom(bottom), _top(top)
     {
-        set_projection_matrix(left, right, bottom, top);
+        set_projection_matrix(_left, _right, _bottom, _top);
     }
 
     void Camera::set_projection_matrix(float left, float right, float bottom, float top)
     {
         _projection_matrix = sf::Transform(2 / (right - left), 0, -(right + left) / (right - left),
-                                           0, -2 / (top - bottom), -(top + bottom) / (top - bottom),
+                                           0, 2 / (top - bottom), -(top + bottom) / (top - bottom),
                                            0, 0, 1);
+
+        _view_projection_matrix = _projection_matrix * _view_matrix;
+    }
+
+    void Camera::set_view_matrix(const sf::Vector2f& position, float rotation)
+    {
+        float radians = rotation * M_PI / 180.0f;
+        float sin = std::sinf(radians);
+        float cos = std::cosf(radians);
+
+        _view_matrix = sf::Transform(cos, -sin, position.x,
+                                     sin, cos, position.y,
+                                     0, 0, 1).getInverse();
+
+        _view_projection_matrix = _projection_matrix * _view_matrix;
     }
 
     void Camera::set_position(sf::Vector2f pos)
     {
         _position = std::move(pos);
+        set_view_matrix(_position, _rotation);
     }
 
     void Camera::move(const sf::Vector2f& vec)
     {
-        _position += vec;
+        set_position(_position + vec);
     }
 
-    const sf::Vector2f& Camera::get_position() const
+    inline const sf::Vector2f& Camera::get_position() const
     {
         return _position;
     }
 
-    void Camera::set_angle(float angle)
+     void Camera::set_rotation(float rotation)
     {
-        _angle = angle;
+        _rotation = rotation;
+        set_view_matrix(_position, _rotation);
     }
 
-    void Camera::rotate(float angle)
+    void Camera::rotate(float rotation)
     {
-        _angle += angle;
+        set_rotation(_rotation + rotation);
     }
 
     void Camera::set_zoom(float zoom)
     {
         _zoom = zoom;
+        set_projection_matrix(_left * _zoom, _right * _zoom, _bottom * _zoom, _top * _zoom);
     }
 
     void Camera::zoom(float zoom)
     {
-        _zoom += zoom;
+        set_zoom(_zoom + zoom);
     }
 
-    Camera::Matrix3 Camera::get_view_projection_matrix() const
+    const Camera::Matrix3& Camera::get_projection_matrix() const
     {
-        Matrix3 id;
-        Matrix3 view = id.translate(_position).rotate(_angle).scale(_zoom, _zoom).getInverse();
-
-        return _projection_matrix * view;
+        return _projection_matrix;
     }
 
-    Camera::Matrix3 Camera::get_rotation() const
+    const Camera::Matrix3& Camera::get_view_matrix() const
     {
-        Matrix3 id;
-        return id.rotate(_angle).scale(_zoom, _zoom);
+        return _view_matrix;
+    }
+
+    const Camera::Matrix3& Camera::get_view_projection_matrix() const
+    {
+        return _view_projection_matrix;
     }
 }
--- core/camera.hpp
+++ core/camera.hpp
@@ -14,25 +14,30 @@ namespace Aporia
         Camera(float left, float right, float bottom, float top);
 
         void set_projection_matrix(float left, float right, float bottom, float top);
+        void set_view_matrix(const sf::Vector2f& position, float rotation);
 
         void set_position(sf::Vector2f pos);
         void move(const sf::Vector2f& vec);
         const sf::Vector2f& get_position() const;
 
-        void set_angle(float angle);
-        void rotate(float angle);
+        void set_rotation(float rotation);
+        void rotate(float rotation);
 
         void set_zoom(float zoom);
         void zoom(float zoom);
 
-        Matrix3 get_view_projection_matrix() const;
-        Matrix3 get_rotation() const;
+        const Matrix3& get_projection_matrix() const;
+        const Matrix3& get_view_matrix() const;
+        const Matrix3& get_view_projection_matrix() const;
 
     private:
         Matrix3 _projection_matrix;
+        Matrix3 _view_matrix;
+        Matrix3 _view_projection_matrix;
 
+        float _left, _right, _bottom, _top;
         sf::Vector2f _position = { 0.0f, 0.0f };
-        float _angle = 0.0f;
+        float _rotation = 0.0f;
         float _zoom = 1.0f;
     };
 }
