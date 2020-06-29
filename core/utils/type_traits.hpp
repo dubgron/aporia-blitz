a08bc6d0c38d2f734f7adc86a47d61886e7a1f61
Patryk Skwiercz <mrdubgron@gmail.com>
2020-06-29 15:42:01 +0200

    refactor: Renamed and simplified type_traits

--- core/utils/type_traits.hpp
+++ core/utils/type_traits.hpp
@@ -5,12 +5,12 @@
 
 namespace Aporia
 {
-    template<typename T, typename Tuple>
-    struct is_in_tuple;
+    template<typename Tuple, typename T>
+    struct has_type;
 
     template<typename T, typename... Us>
-    struct is_in_tuple<T, std::tuple<Us...>> : std::disjunction<std::is_same<T, Us>...> {};
+    struct has_type<std::tuple<Us...>, T> : std::disjunction<std::is_same<T, Us>...> {};
     
-    template<typename T, typename... Us>
-    constexpr bool is_in_tuple_v = is_in_tuple<T, Us...>::value;
+    template<typename Tuple, typename T>
+    constexpr bool has_type_v = has_type<Tuple, T>::value;
 }
