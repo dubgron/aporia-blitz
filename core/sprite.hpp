7cccc68f9c7f4e7e4ed81677f3f6c61c54d1d720
Patryk Skwiercz <mrdubgron@gmail.com>
2020-07-03 15:56:11 +0200

    feat: Added Color and Rectangular components to Sprite

--- core/sprite.hpp
+++ core/sprite.hpp
@@ -4,18 +4,21 @@
 
 #include "entity.hpp"
 #include "logger.hpp"
+#include "components/color.hpp"
+#include "components/rectangular.hpp"
 #include "components/texture.hpp"
 #include "components/transform2d.hpp"
 
 namespace Aporia
 {
-    struct Sprite final : public Entity<Transform2D, Texture>
+    struct Sprite final : public Entity<Transform2D, Texture, Rectangular, Color>
     {
         Sprite() = default;
 
-        Sprite(const Texture& texture, sf::Vector2f position = { 0.0f, 0.0f })
-            : Entity(Transform2D{ position, 0.0, { position.x + texture.width / 2.0f, position.y + texture.height / 2.0f } }, Texture{ texture })
-        {
-        }
+        Sprite(const Texture& texture, sf::Vector2f position = { 0.0f, 0.0f }, Color color = Colors::White)
+            : Entity(Transform2D{ position, 0.0, { position.x + texture.width / 2.0f, position.y + texture.height / 2.0f } },
+                     Texture{ texture },
+                     Rectangular{ texture.width, texture.height },
+                     Color{ std::move(color) }) {}
     };
 }
