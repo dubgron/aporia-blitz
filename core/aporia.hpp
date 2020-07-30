e9d6d024e66746efcd4e6efc5bcf7d26198720cd
Patryk Skwiercz <mrdubgron@gmail.com>
2020-07-30 23:47:05 +0200

    feat: Added AnimationConfig

--- core/aporia.hpp
+++ core/aporia.hpp
@@ -1,5 +1,6 @@
 /* Configs */
 #include <configs/all_configs.hpp>
+#include <configs/animation_config.hpp>
 #include <configs/camera_config.hpp>
 #include <configs/texture_config.hpp>
 #include <configs/window_config.hpp>
--- core/configs/all_configs.hpp
+++ core/configs/all_configs.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "configs/animation_config.hpp"
 #include "configs/camera_config.hpp"
 #include "configs/texture_config.hpp"
 #include "configs/window_config.hpp"
