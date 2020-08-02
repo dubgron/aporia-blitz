108291f3924191a453f125d6d53a6cf04e5de167
Patryk Skwiercz <mrdubgron@gmail.com>
2020-08-02 22:11:25 +0200

    refactor: Moved load_textures functionality to a contructor

--- core/game.cpp
+++ core/game.cpp
@@ -5,7 +5,7 @@ namespace Aporia
     Game::Game(const std::string& config_file)
         : _logger("engine"),
           _configs(_logger, config_file),
-          _textures(_logger),
+          _textures(_logger, _configs.texture_config),
           _events(_logger),
           _inputs(_logger, _events),
           _scenes(_logger),
@@ -16,9 +16,6 @@ namespace Aporia
           _imgui_layer(_logger, _window)
     {
         if (!_configs.is_good())
-            return;
-
-        if (!_textures.load_textures(_configs.texture_config))
             return;
 
         _events.add_listener<WindowCloseEvent>([](Window& window) { window.close(); });
