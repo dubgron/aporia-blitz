ca88ceb544dca96f7f2b8ac323d2c5e099ea81b7
Patryk Skwiercz <mrdubgron@gmail.com>
2020-10-17 19:57:59 +0200

    feat: Added more random distributions

--- core/utils/random.hpp
+++ core/utils/random.hpp
@@ -5,15 +5,54 @@
 
 namespace Aporia
 {
-    template<typename T>
-    inline T random(T min, T max)
+    namespace impl
     {
-        static std::mt19937 gen{ std::random_device{}() };
+        template<typename Dist, typename... Ts>
+        inline auto random(Ts... args)
+        {
+            static std::mt19937 gen{ std::random_device{}() };
+            return Dist{ args... }(gen);
+        }
+    }
 
-        if constexpr (std::is_integral_v<T>)
-            return std::uniform_int_distribution<T>{min, max}(gen);
+    template<typename T>
+    inline T uniform(T min, T max) requires std::is_integral_v<T>
+    {
+        return impl::random<std::uniform_int_distribution<T>>(min, max);
+    }
 
-        if constexpr (std::is_floating_point_v<T>)
-            return std::uniform_real_distribution<T>{min, max}(gen);
+    template<typename T>
+    inline T uniform(T min, T max) requires std::is_floating_point_v<T>
+    {
+        return impl::random<std::uniform_real_distribution<T>>(min, max);
+    }
+
+    template<typename T>
+    inline T normal(T mean, T stddev) requires std::is_floating_point_v<T>
+    {
+        return impl::random<std::normal_distribution<T>>(mean, stddev);
+    }
+
+    template<typename T>
+    inline T exponential(T lambda) requires std::is_floating_point_v<T>
+    {
+        return impl::random<std::exponential_distribution<T>>(lambda);
+    }
+
+    template<typename T = int>
+    inline T poisson(float mean) requires std::is_integral_v<T>
+    {
+        return impl::random<std::poisson_distribution<T>>(mean);
+    }
+
+    template<typename T>
+    inline T binomial(T n, float p) requires std::is_integral_v<T>
+    {
+        return impl::random<std::binomial_distribution<T>>(n, p);
+    }
+
+    inline bool bernoulli(float p)
+    {
+        return impl::random<std::bernoulli_distribution>(p);
     }
 }
