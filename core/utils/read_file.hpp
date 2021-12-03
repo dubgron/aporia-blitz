6f1eaad83c7746217856839636af9ebe2368c574
Patryk Skwiercz <mrdubgron@gmail.com>
2021-12-03 23:23:39 +0100

    feat: Made read_file function use std::string_view

--- core/utils/read_file.hpp
+++ core/utils/read_file.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 #include <fstream>
-#include <string>
+#include <string_view>
 #include <utility>
 
 namespace Aporia
 {
-    inline std::string read_file(const std::string& path)
+    inline std::string read_file(std::string_view path)
     {
-        std::ifstream file(path, std::ios::binary);
+        std::ifstream file(path.data(), std::ios::binary);
         return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     }
 }
