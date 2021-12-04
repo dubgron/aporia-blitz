1a61f7c6a8917e68c063df77b6713725e1e7fdfe
Patryk Skwiercz <mrdubgron@gmail.com>
2021-12-05 00:10:32 +0100

    feat: Added utility function to load json files

--- core/utils/read_file.hpp
+++ core/utils/read_file.hpp
@@ -4,11 +4,19 @@
 #include <string_view>
 #include <utility>
 
+#include <nlohmann/json.hpp>
+
 namespace Aporia
 {
     inline std::string read_file(std::string_view path)
     {
         std::ifstream file(path.data(), std::ios::binary);
         return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    }
+
+    inline nlohmann::json load_json(std::string_view path)
+    {
+        std::string data = read_file(path);
+        return nlohmann::json::parse(std::move(data));
     }
 }
