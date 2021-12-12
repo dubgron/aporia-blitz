8c6b35bd839569d4326bec49f2b259dbeede7671
Patryk Skwiercz <mrdubgron@gmail.com>
2021-12-12 18:12:09 +0100

    bugfix: Disabled OpenGL logging for Emscripten build

--- core/graphics/opengl.hpp
+++ core/graphics/opengl.hpp
@@ -12,40 +12,38 @@
 #   define OPENGL_VERSION_MINOR 5
 #   define OPENGL_MAX_TEXTURE_UNITS 32
 #   include <GL/gl3w.h>
+
+    namespace Aporia
+    {
+        constexpr const char* get_debug_source(GLenum source)
+        {
+            switch (source)
+            {
+            case GL_DEBUG_SOURCE_API:                   return "OPENGL_API";
+            case GL_DEBUG_SOURCE_WINDOW_SYSTEM:         return "WINDOW_SYSTEM";
+            case GL_DEBUG_SOURCE_SHADER_COMPILER:       return "SHADER_COMPILER";
+            case GL_DEBUG_SOURCE_THIRD_PARTY:           return "THIRD_PARTY";
+            case GL_DEBUG_SOURCE_APPLICATION:           return "APPLICATION";
+            case GL_DEBUG_SOURCE_OTHER:                 return "OTHER";
+            default:                                    return "NONE";
+            }
+        }
+
+        constexpr const char* get_debug_type(GLenum type)
+        {
+            switch (type)
+            {
+            case GL_DEBUG_TYPE_ERROR:                   return "ERROR";
+            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:     return "DEPRECATED_BEHAVIOR";
+            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:      return "UNDEFINED_BEHAVIOR";
+            case GL_DEBUG_TYPE_PORTABILITY:             return "PORTABILITY";
+            case GL_DEBUG_TYPE_PERFORMANCE:             return "PERFORMANCE";
+            case GL_DEBUG_TYPE_MARKER:                  return "MARKER";
+            case GL_DEBUG_TYPE_PUSH_GROUP:              return "PUSH_GROUP";
+            case GL_DEBUG_TYPE_POP_GROUP:               return "POP_GROUP";
+            case GL_DEBUG_TYPE_OTHER:                   return "OTHER";
+            default:                                    return "NONE";
+            }
+        }
+    }
 #endif
-
-#include <string_view>
-
-namespace Aporia
-{
-    constexpr std::string_view get_debug_source(GLenum source)
-    {
-        switch (source)
-        {
-        case GL_DEBUG_SOURCE_API:                   return "OPENGL_API";
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:         return "WINDOW_SYSTEM";
-        case GL_DEBUG_SOURCE_SHADER_COMPILER:       return "SHADER_COMPILER";
-        case GL_DEBUG_SOURCE_THIRD_PARTY:           return "THIRD_PARTY";
-        case GL_DEBUG_SOURCE_APPLICATION:           return "APPLICATION";
-        case GL_DEBUG_SOURCE_OTHER:                 return "OTHER";
-        default:                                    return "NONE";
-        }
-    }
-
-    constexpr std::string_view get_debug_type(GLenum type)
-    {
-        switch (type)
-        {
-        case GL_DEBUG_TYPE_ERROR:                   return "ERROR";
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:     return "DEPRECATED_BEHAVIOR";
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:      return "UNDEFINED_BEHAVIOR";
-        case GL_DEBUG_TYPE_PORTABILITY:             return "PORTABILITY";
-        case GL_DEBUG_TYPE_PERFORMANCE:             return "PERFORMANCE";
-        case GL_DEBUG_TYPE_MARKER:                  return "MARKER";
-        case GL_DEBUG_TYPE_PUSH_GROUP:              return "PUSH_GROUP";
-        case GL_DEBUG_TYPE_POP_GROUP:               return "POP_GROUP";
-        case GL_DEBUG_TYPE_OTHER:                   return "OTHER";
-        default:                                    return "NONE";
-        }
-    }
-}
--- core/window.cpp
+++ core/window.cpp
@@ -87,25 +87,27 @@ namespace Aporia
 
         _logger.log(LOG_INFO) << glGetString(GL_VERSION);
 
-        glEnable(GL_DEBUG_OUTPUT);
-        glDebugMessageCallback([](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
-            {
-                const Aporia::Logger* logger = reinterpret_cast<const Aporia::Logger*>(userParam);
-                const auto get_logger = [&logger](GLenum severity)
+#       if !defined(APORIA_EMSCRIPTEN)
+            glEnable(GL_DEBUG_OUTPUT);
+            glDebugMessageCallback([](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
                 {
-                    switch (severity)
+                    const Aporia::Logger* logger = reinterpret_cast<const Aporia::Logger*>(userParam);
+                    const auto get_logger = [&logger](GLenum severity)
                     {
-                    case GL_DEBUG_SEVERITY_HIGH:            return logger->log(LOG_ERROR);
-                    case GL_DEBUG_SEVERITY_MEDIUM:          return logger->log(LOG_WARNING);
-                    case GL_DEBUG_SEVERITY_LOW:             return logger->log(LOG_WARNING);
-                    case GL_DEBUG_SEVERITY_NOTIFICATION:    return logger->log(LOG_DEBUG);
-                    default:                                return logger->log(LOG_ERROR);
-                    }
-                };
+                        switch (severity)
+                        {
+                        case GL_DEBUG_SEVERITY_HIGH:            return logger->log(LOG_ERROR);
+                        case GL_DEBUG_SEVERITY_MEDIUM:          return logger->log(LOG_WARNING);
+                        case GL_DEBUG_SEVERITY_LOW:             return logger->log(LOG_WARNING);
+                        case GL_DEBUG_SEVERITY_NOTIFICATION:    return logger->log(LOG_DEBUG);
+                        default:                                return logger->log(LOG_ERROR);
+                        }
+                    };
 
-                get_logger(severity) << get_debug_source(source) << " " << get_debug_type(type) << " [ID=" << id << "] \"" << message << "\"";
-            },
-            reinterpret_cast<const void*>(&_logger));
+                    get_logger(severity) << get_debug_source(source) << " " << get_debug_type(type) << " [ID=" << id << "] \"" << message << "\"";
+                },
+                reinterpret_cast<const void*>(&_logger));
+#       endif
 
         events.add_listener<WindowCloseEvent>([](Window& window)
             {
