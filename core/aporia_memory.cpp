9558662b2f79d97071906349e3b465d732cf4eaa
Patryk Skwiercz <mrdubgron@gmail.com>
2023-09-02 21:55:45 +0200

    feat: Added use-after-free memory detection for ScratchArena

--- core/aporia_memory.cpp
+++ core/aporia_memory.cpp
@@ -67,6 +67,27 @@ namespace Aporia
     void rollback_scratch_arena(ScratchArena& scratch)
     {
         APORIA_ASSERT_WITH_MESSAGE(scratch.arena, "Scratch arena has an invalid arena pointer! Can't rollback!");
+
+#if defined(APORIA_DEBUGTOOLS)
+        APORIA_ASSERT(scratch.arena->pos >= scratch.pos);
+
+        // @NOTE(dubgron): Using scratch arenas enables a peculiar use-after-free
+        // bug, where you create a scratch arena on top of arena X, you push some
+        // values to it (which are meant to be temporary), you push some values to
+        // arena X (which are not meant to be temporary), and you free the scratch
+        // arena at the end. In this case, the values pushed directly onto arena X
+        // would be treated as freed, and could be potentially overwritten later.
+        // These sorts of bugs are near impossible to catch (without using static
+        // code analysis), but we can make them easier to detect. In debug builds,
+        // we clear the memory used by a scratch arena with a value of 0xae and
+        // thus making the program either crash immediately (making tracking the
+        // source of the bug easier) or have a simple to spot pattern in memory.
+
+        void* scratch_block_begin = INT_TO_PTR(PTR_TO_INT(scratch.arena->memory) + scratch.pos);
+        u64 scratch_block_size = scratch.arena->pos - scratch.pos;
+        memset(scratch_block_begin, 0xae, scratch_block_size);
+#endif
+
         scratch.arena->pos = scratch.pos;
     }
 }
